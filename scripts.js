6e40a547aae961666b78c3e10eba4d053cf695ee
Mohammed Aeraf <aeraf@outlook.com>
2025-11-24 21:08:23 +0530

    JavaScript Intro

---
+++ scripts.js
@@ -0,0 +1,7 @@
+function changePara() {
+  let p1 = document.getElementById("p1");
+  p1.style.color = "brown";
+  p1.style.border = "1px solid red";
+  p1.style.borderRadius = "5px";
+  p1.style.backgroundColor = "bisque";
+}
